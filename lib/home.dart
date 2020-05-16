235193518ad9cd22210d7cbad90d631cbeaf084c
Minh To <minhqto3@gmail.com>
2020-05-15 21:43:47 -0400

    formatting changes

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+
 //import "./widgets/bottomMenuBar.dart";
 import "package:mypalate/views/mylist.dart";
 import 'package:mypalate/views/homePage.dart';
@@ -10,93 +11,82 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
 
-  int _selectedIndex = 0 ;
-
-  void _onItemTapped(int index){
+  void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
-      body: Stack(
-        children: <Widget>[
-          Container(child: new IndexedStack(
-            index: _selectedIndex,
-            children: <Widget>[
-               HomePage(),
-               MyList(),
-              Discover()
-            ]
-          )),
-          Positioned.fill(
-            child: LayoutBuilder(builder: (context, constraints){
-
-          return Padding(
-          padding: EdgeInsets.only(
-              top: constraints.biggest.height * .83,
-              bottom: constraints.biggest.height * .1,
-              left: constraints.biggest.width * .05,
-              right: constraints.biggest.width * .05),
-          child: ClipRRect(
-          borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(25),
-          topRight: Radius.circular(25),
-          bottomLeft: Radius.circular(25),
-          bottomRight: Radius.circular(25)
-          ),
-          child: BottomNavigationBar(
-          onTap: _onItemTapped,
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.white,
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.amber,
-
-          items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text("Home", style: TextStyle(fontSize: 10, color: Colors.black)),
-
-          ),
-          BottomNavigationBarItem(
-          icon: Icon(Icons.format_list_numbered),
-          title: Text("My List", style: TextStyle(fontSize: 10, color: Colors.black)),
-          ),
-
-          BottomNavigationBarItem(
-          icon: Icon(Icons.remove_red_eye),
-          title: Text("Discover", style: TextStyle(fontSize: 10, color: Colors.black)),
-
-          ),
-          BottomNavigationBarItem(
-          icon: Icon(Icons.fastfood),
-          title: Text("Meals", style: TextStyle(fontSize: 10, color: Colors.black)),
-          ),
-
-          BottomNavigationBarItem(
-          icon: Icon(Icons.account_circle),
-          title: Text("Account", style: TextStyle(fontSize: 10, color: Colors.black)),
-          ),
-
+        appBar: null,
+        body: Stack(
+          children: <Widget>[
+            Container(
+                child: new IndexedStack(
+                    index: _selectedIndex,
+                    children: <Widget>[HomePage(), MyList(), Discover()])),
+            Positioned.fill(
+              child: LayoutBuilder(builder: (context, constraints) {
+                return Padding(
+                  padding: EdgeInsets.only(
+                      top: constraints.biggest.height * .83,
+                      bottom: constraints.biggest.height * .1,
+                      left: constraints.biggest.width * .05,
+                      right: constraints.biggest.width * .05),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25),
+                        bottomLeft: Radius.circular(25),
+                        bottomRight: Radius.circular(25)),
+                    child: BottomNavigationBar(
+                      onTap: _onItemTapped,
+                      currentIndex: _selectedIndex,
+                      selectedItemColor: Colors.white,
+                      type: BottomNavigationBarType.fixed,
+                      backgroundColor: Colors.amber,
+                      items: const <BottomNavigationBarItem>[
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.home),
+                          title: Text("Home",
+                              style:
+                                  TextStyle(fontSize: 10, color: Colors.black)),
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.format_list_numbered),
+                          title: Text("My List",
+                              style:
+                                  TextStyle(fontSize: 10, color: Colors.black)),
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.remove_red_eye),
+                          title: Text("Discover",
+                              style:
+                                  TextStyle(fontSize: 10, color: Colors.black)),
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.fastfood),
+                          title: Text("Meals",
+                              style:
+                                  TextStyle(fontSize: 10, color: Colors.black)),
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.account_circle),
+                          title: Text("Account",
+                              style:
+                                  TextStyle(fontSize: 10, color: Colors.black)),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }),
+            )
           ],
-          ),
-          ),
-          );
-          }
-          ),
-//          Positioned(
-//            left: 0,
-//            right: 0,
-//            bottom: 100,
-//              child:
-//                    )
-          )],
-      )
-    );
+        ));
   }
 }
--- lib/widgets/miniRecipeCard.dart
+++ lib/widgets/miniRecipeCard.dart
@@ -10,17 +10,14 @@ class _MiniRecipeCardState extends State<MiniRecipeCard> {
   @override
   Widget build(BuildContext context) {
     return Card(
-
-      elevation: 1,
-      child: ListTile(
-        onTap: (){},
-          //TODO opens up the recipe card for that list tile,
-        title: Text("This is a recipe!"),
-        leading:Image(
-          image: NetworkImage('https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/154660.jpg'),
-        )
-
-      )
-    );
+        elevation: 1,
+        child: ListTile(
+            onTap: () {},
+            //TODO opens up the recipe card for that list tile,
+            title: Text("This is a recipe!"),
+            leading: Image(
+              image: NetworkImage(
+                  'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/154660.jpg'),
+            )));
   }
 }
