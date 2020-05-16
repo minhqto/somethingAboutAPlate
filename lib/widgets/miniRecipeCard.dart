c5812f876c074237841076894f455e0d0929163d
viet-quocnguyen <nguyenquocviet170898@gmail.com>
2020-05-15 21:36:53 -0400

    create recipe card component

--- lib/widgets/miniRecipeCard.dart
+++ lib/widgets/miniRecipeCard.dart
@@ -9,18 +9,146 @@ class MiniRecipeCard extends StatefulWidget {
 class _MiniRecipeCardState extends State<MiniRecipeCard> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-
-      elevation: 1,
-      child: ListTile(
-        onTap: (){},
-          //TODO opens up the recipe card for that list tile,
-        title: Text("This is a recipe!"),
-        leading:Image(
-          image: NetworkImage('https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/154660.jpg'),
-        )
-
-      )
+    return Container(
+      margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+      height: 160,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black,
+            offset: Offset(0, 2),
+            blurRadius: 6.0,
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(10.0, 10.0, 20.0, 10.0),
+        child: Row(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image(
+                width: 110.0,
+                image: AssetImage('assets/img/food-demo.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        RichText(
+                          text: TextSpan(
+                            style: Theme.of(context).textTheme.body1,
+                            children: [
+                              TextSpan(text: '4.5'),
+                              WidgetSpan(
+                                child: Padding(
+                                  padding:
+                                      EdgeInsets.symmetric(horizontal: 5.0),
+                                  child: Icon(
+                                    Icons.star,
+                                    size: 18.0,
+                                    color: Colors.orange,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Icon(
+                          Icons.bookmark,
+                          size: 18.0,
+                          color: Colors.green,
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Recipe Name here",
+                          style: TextStyle(fontSize: 22.0),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 0.0, vertical: 10.0),
+                          child: Container(
+                            height: 1.0,
+                            width: 200.0,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            style: Theme.of(context).textTheme.body1,
+                            children: [
+                              WidgetSpan(
+                                child: Padding(
+                                  padding:
+                                      EdgeInsets.symmetric(horizontal: 5.0),
+                                  child: Icon(
+                                    Icons.timer,
+                                    size: 18.0,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                              TextSpan(
+                                  text: '30 mins',
+                                  style: TextStyle(color: Colors.grey)),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        RichText(
+                          text: TextSpan(
+                            style: Theme.of(context).textTheme.body1,
+                            children: [
+                              WidgetSpan(
+                                child: Padding(
+                                  padding:
+                                      EdgeInsets.symmetric(horizontal: 5.0),
+                                  child: Icon(
+                                    Icons.warning,
+                                    size: 18.0,
+                                    color: Colors.red,
+                                  ),
+                                ),
+                              ),
+                              TextSpan(
+                                  text: '3 warnings',
+                                  style: TextStyle(color: Colors.red)),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
